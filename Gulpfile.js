c32e047bbb1bb69405446620f445fe2270ac00bb
achrafsallemi <achrafsallemi@yahoo.fr>
2017-02-27 16:43:26 +0100

    Cleaned code

--- Gulpfile.js
+++ Gulpfile.js
@@ -8,7 +8,7 @@ gulp.task('lint', function() {
     return gulp.src('web/js/*.js')
         .pipe(jshint())
         .pipe(jshint.reporter('default'))
-        ;
+    ;
 });
 
 gulp.task('default', ['lint']);
--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -18,15 +18,4 @@ class DefaultController extends Controller
             'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
         ]);
     }
-
-    /**
-     * @Route("/blog", name="blogpage")
-     */
-    public function blogAction(Request $request)
-    {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
-        ]);
-    }
 }
